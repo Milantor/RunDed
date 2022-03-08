8c946482ff56f7635a9c8e27c950a269dd20c17d
Milantor <nikitaistra.ru@gmail.com>
2022-03-08 22:10:11 +0300

    еее рок гитлер не виноват путин мем мама не смотри сюда

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//этот скрипт отвечает за передвижение GO персонажа в мире
+
+public class Movement : MonoBehaviour
+{
+    public decimal speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
