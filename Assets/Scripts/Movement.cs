4705a1d88ff4d19230f09132b556605590c32771
Milantor <nikitaistra.ru@gmail.com>
2022-03-09 11:30:24 +0300

    теперь можно ходить не кринжуя

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public decimal speed;
+    public float Hspeed; //скорость(горизонтальная)
+    public float Vspeed; //скорость(вертикальная)
+    public float speedModificator = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,21 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        #region basic move
+        Vector3 velocity = Vector3.zero;
+        switch (Input.GetAxis("Horizontal")){
+            case > 0:
+                Hspeed = 1;
+                break;
+            case < 0:
+                Hspeed = -1;
+                break;
+            case 0: 
+                Hspeed = 0;
+                break;
+        }
+        velocity += new Vector3(Hspeed * speedModificator, Vspeed * speedModificator, 0);
+        transform.position += velocity;
+        #endregion
     }
 }
