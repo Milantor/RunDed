c0a1da24321b6b2ae287f9c75990115ec56bbbdc
CREAsTIVE <62616308+CREAsTIVE@users.noreply.github.com>
2023-03-29 17:05:42 +0500

    small refactor x2
    Хватит кринжевать

--- Assets/Scripts/InventorySystem/InventoryCell.cs
+++ Assets/Scripts/InventorySystem/InventoryCell.cs
@@ -3,41 +3,34 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 /// <summary>
-/// Класс, отвечающий за физические объекты клеток инвентаря
+/// ГЉГ«Г Г±Г±, Г®ГІГўГҐГ·Г ГѕГ№ГЁГ© Г§Г  ГґГЁГ§ГЁГ·ГҐГ±ГЄГЁГҐ Г®ГЎГєГҐГЄГІГ» ГЄГ«ГҐГІГ®ГЄ ГЁГ­ГўГҐГ­ГІГ Г°Гї
 /// </summary>
 public class InventoryCell : MonoBehaviour, IPointerClickHandler
 {
     /// <summary>
-    /// Предмет, хранящийся в клетке
+    /// ГЏГ°ГҐГ¤Г¬ГҐГІ, ГµГ°Г Г­ГїГ№ГЁГ©Г±Гї Гў ГЄГ«ГҐГІГЄГҐ
     /// </summary>
     public Item item;
     /// <summary>
-    /// ID клетки
+    /// ID ГЄГ«ГҐГІГЄГЁ
     /// </summary>
     public int ID;
 
     private Image _image;
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        DraDr._instance.Click(this);
-        Debug.Log(string.Format("Clicked cell id: {0}\nClicked item id: {1}", ID.ToString(), item.id));
-    }
+    public void OnPointerClick(PointerEventData eventData) => DraDr._instance.Click(this);
+        //Debug.Log(string.Format("Clicked cell id: {0}\nClicked item id: {1}", ID.ToString(), item.id));
 
     // ReSharper disable Unity.PerformanceAnalysis
     /// <summary>
-    /// Обновляет данные о клетке
+    /// ГЋГЎГ­Г®ГўГ«ГїГҐГІ Г¤Г Г­Г­Г»ГҐ Г® ГЄГ«ГҐГІГЄГҐ
     /// </summary>
     public void UpdateCell()
     {
         _image = transform.GetChild(0).GetComponent<Image>();
         if (item == null)
-        {
             item = new Item();
-        }
         else
-        {
             _image.sprite = item.sprite;
-        }
     }
 }
